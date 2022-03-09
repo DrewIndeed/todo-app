4df140ba87f470106bec175669931768068d62a2
Andrew Le <s3820098@rmit.edu.vn>
2022-03-09 20:04:21 +0700

    7. populate css variables, font size and font family

--- react-application/src/components/BackgroundContainer/BackgroundContainer.jsx
+++ react-application/src/components/BackgroundContainer/BackgroundContainer.jsx
@@ -4,12 +4,12 @@ import { Images } from '../../assets/index';
 
 const BackgroundContainer = () => {
   return (
-    <div className="bg-blue-500 w-full h-screen flex flex-col">
+    <div className="w-full h-screen flex flex-col">
       <div
         style={{ backgroundImage: `url(${Images.desktopDark})` }}
         className="top-background"
       ></div>
-      <div className="bot-background bg-green-500 flex-1"></div>
+      <div className="bot-background flex-1"></div>
     </div>
   );
 };
