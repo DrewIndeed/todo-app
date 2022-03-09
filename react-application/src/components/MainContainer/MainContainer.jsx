43c5b781677bbb048d891a4e90d4c45090603389
bmhuyquoc104 <56634994+bmhuyquoc104@users.noreply.github.com>
2022-03-09 20:33:40 +0700

    9. Add absolute (transparent) container + card container

--- react-application/src/components/MainContainer/MainContainer.jsx
+++ react-application/src/components/MainContainer/MainContainer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './mainContainer.css'
-
 const MainContainer = () => {
-  return <div>MainContainer</div>;
+  return <div className ="main-container">
+      <div className="card-container">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis distinctio molestias. Consequatur ipsa, placeat libero, quisquam ea obcaecati vel ratione itaque, qui fugit perferendis?
+        </div>
+    </div>;
 };
 
 export default MainContainer;
