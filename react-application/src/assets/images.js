0144ec1f26152caf10a444f65878d46848e2e942
Andrew Le <s3820098@rmit.edu.vn>
2022-03-09 19:51:36 +0700

    6. Optimized images imports

---
+++ react-application/src/assets/images.js
@@ -0,0 +1,21 @@
+import desktopDark from './images/bg-desktop-dark.jpg';
+import desktopLight from './images/bg-desktop-light.jpg';
+import mobileDark from './images/bg-mobile-dark.jpg';
+import mobileLight from './images/bg-mobile-light.jpg';
+import checkIcon from './images/icon-check.svg';
+import crossIcon from './images/icon-cross.svg';
+import moonIcon from './images/icon-moon.svg';
+import sunIcon from './images/icon-sun.svg';
+
+const allImages = {
+  desktopDark,
+  desktopLight,
+  mobileDark,
+  mobileLight,
+  checkIcon,
+  crossIcon,
+  moonIcon,
+  sunIcon,
+};
+
+export default allImages;
--- react-application/src/components/BackgroundContainer/BackgroundContainer.jsx
+++ react-application/src/components/BackgroundContainer/BackgroundContainer.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './backgroundContainer.css';
+import { Images } from '../../assets/index';
 
 const BackgroundContainer = () => {
-  return <div className="bg-blue-500 w-full h-screen flex flex-col">
-    <div className="top-background bg-red-500">
-
-      
+  return (
+    <div className="bg-blue-500 w-full h-screen flex flex-col">
+      <div
+        style={{ backgroundImage: `url(${Images.desktopDark})` }}
+        className="top-background"
+      ></div>
+      <div className="bot-background bg-green-500 flex-1"></div>
     </div>
-    <div className="bot-background bg-green-500 flex-1">
-
-    </div>
-  </div>;
+  );
 };
 
 export default BackgroundContainer;
