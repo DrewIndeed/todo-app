b7478497668471ab1691398b78d470eda201fcc8
Andrew Le Nguyen <truongan1730@gmail.com>
2022-03-09 14:22:31 +0700

    5. Background testings

--- react-application/src/App.jsx
+++ react-application/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import BackgroundContainer from './components/BackgroundContainer/BackgroundContainer';
 
 const App = () => {
-  return <div className="app">This is Todo App</div>;
+  return (
+    <div className="app">
+      <BackgroundContainer />
+    </div>
+  );
 };
 
 export default App;
--- react-application/src/components/BackgroundContainer/BackgroundContainer.jsx
+++ react-application/src/components/BackgroundContainer/BackgroundContainer.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './backgroundContainer.css';
 
 const BackgroundContainer = () => {
-  return <div>BackgroundContainer</div>;
+  return <div className="bg-blue-500 w-full h-screen flex flex-col">
+    <div className="top-background bg-red-500">
+
+      
+    </div>
+    <div className="bot-background bg-green-500 flex-1">
+
+    </div>
+  </div>;
 };
 
 export default BackgroundContainer;
