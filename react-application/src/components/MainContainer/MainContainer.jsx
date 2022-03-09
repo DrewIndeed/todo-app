31528c737246329f676a14c6da6452427e613dc3
Andrew Le <s3820098@rmit.edu.vn>
2022-03-09 21:07:17 +0700

    11. render drag and drop message and space for items

--- react-application/src/components/MainContainer/MainContainer.jsx
+++ react-application/src/components/MainContainer/MainContainer.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import './mainContainer.css'
-import {Images} from '../../assets/index'
+import './mainContainer.css';
+import { Images } from '../../assets/index';
 const MainContainer = () => {
-  return <div className ="main-container">
+  return (
+    <div className="main-container">
       <div className="card-container">
-          <div className="card-image">
-            <h2>TODO</h2>
-            <div className="image">
-              <img src={Images.sunIcon} alt="A sun icon" />
-              </div>
+        {/* main title */}
+        <div className="card-image">
+          <h2>TODO</h2>
+          <div className="image">
+            <img src={Images.sunIcon} alt="A sun icon" />
           </div>
         </div>
-    </div>;
+
+        {/* new item input and items list */}
+        <div className="flex-1 bg-orange-600"></div>
+        {/* drag and drop msg */}
+        <div className="bg-transparent flex items-center justify-center py-12">
+          <h3 className="drag-drop-msg text-xs font-bold">
+            Drag and drop to reorder list
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainContainer;
